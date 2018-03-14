a8ac0312108af19512ac125a94f29972ea52a60d
c9katayama <c95029@gmail.com>
2018-03-14 18:14:30 +0900

    fix bootstrap url

--- soracom-inventory-agent-for-java-core/src/main/java/io/soracom/inventory/agent/core/bootstrap/BootstrapConstants.java
+++ soracom-inventory-agent-for-java-core/src/main/java/io/soracom/inventory/agent/core/bootstrap/BootstrapConstants.java
@@ -17,6 +17,6 @@ package io.soracom.inventory.agent.core.bootstrap;
 
 public interface BootstrapConstants {
 
-	public static final String DEFAULT_BOOTSTRAP_SERVER_ADDRESS = "100.127.127.100:5683";
-	public static final String DEFAULT_BOOTSTRAP_COAPS_SERVER_ADDRESS = "100.127.127.100:5684";
+	public static final String DEFAULT_BOOTSTRAP_SERVER_ADDRESS = "bootstrap.soracom.io:5683";
+	public static final String DEFAULT_BOOTSTRAP_COAPS_SERVER_ADDRESS = "bootstrap.soracom.io:5684";
 }
